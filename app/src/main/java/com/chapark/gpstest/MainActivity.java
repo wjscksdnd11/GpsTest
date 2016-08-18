d41afba227aabbeeec85d609293c468471618376
jeon <wjscksdnd11@naver.com>
2016-08-18 10:23:18 +0900

    Initial version

---
+++ app/src/main/java/com/chapark/gpstest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chapark.gpstest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
